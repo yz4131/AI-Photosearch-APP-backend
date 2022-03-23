d50a16382aaad5c8bf7e48ee3bc53da24fe5e273
Yuqin Zhao <yz4131@columbia.edu>
2022-03-23 11:34:15 -0400

    lambda

---
+++ index-photos/lambda_function.py
@@ -0,0 +1,51 @@
+from requests_aws4auth import AWS4Auth
+import json
+import urllib.parse
+import boto3
+import requests
+
+
+s3 = boto3.client('s3')
+client=boto3.client('rekognition')
+
+
+def lambda_handler(event, context):
+    # print("Received event: " + json.dumps(event, indent=2))
+
+    # Get the object from the event and show its content type
+    bucket = event['Records'][0]['s3']['bucket']['name']
+    key = urllib.parse.unquote_plus(event['Records'][0]['s3']['object']['key'], encoding='utf-8')
+    labels=[]
+    try:
+        response = s3.get_object(Bucket=bucket, Key=key)
+        temp = s3.head_object(Bucket=bucket, Key=key)
+        # print(temp['x-amz-meta-customLabels'])
+        # print("CONTENT TYPE: " + response['x-amz-meta-customLabels'])
+        predict = client.detect_labels(Image={'S3Object':{'Bucket':bucket,'Name':key}}, MaxLabels=4, MinConfidence=50)
+        for label in predict['Labels']:
+            labels.append(label['Name'])
+        # print(labels)
+        res = {
+            'objectKey': key,
+            'bucket': bucket,
+            'createdTimestamp': temp['ResponseMetadata']['HTTPHeaders']['date'],
+            'labels': labels
+        }
+        # res = json.dumps(res)
+        # print(res)
+
+        # post to es
+        region = 'us-east-1'
+        service = 'es'
+        credentials = boto3.Session().get_credentials()
+        awsauth = AWS4Auth(credentials.access_key, credentials.secret_key, region, service, session_token=credentials.token)
+        host = 'https://search-photosearch-keg5p47cv2pwbkvyvalwgnv7lq.us-east-1.es.amazonaws.com'
+        index = 'photo'
+        type = 'lambda-type'
+        url = host + "/" + index + "/" + type
+        headers = { "Content-Type": "application/json" }
+        r = requests.post(url, auth=awsauth, json=res, headers=headers)
+        print(r.text)
+    except Exception as e:
+        print(e)
+        raise e
---
+++ search-photos/lambda_function.py
@@ -0,0 +1,75 @@
+import math
+import dateutil.parser
+import datetime
+import time
+import boto3
+import json
+import requests
+from requests_aws4auth import AWS4Auth
+import os
+
+
+def searchFromES(keywords):
+    photos = []
+    region = 'us-east-1'
+    service = 'es'
+    credentials = boto3.Session().get_credentials()
+    awsauth = AWS4Auth(credentials.access_key, credentials.secret_key, region, service, session_token=credentials.token)
+
+    host = 'https://search-photosearch-keg5p47cv2pwbkvyvalwgnv7lq.us-east-1.es.amazonaws.com'
+    for key in keywords:
+        index = 'photo'
+        url = host + '/' + index + '/_search'
+        query = {
+            "size": 1000,
+            "query": {
+                "query_string": {
+                "default_field": "labels",
+                "query": key
+                }
+            },
+            "sort":{
+            "_script":{
+                "script":"Math.random()",
+                "type":"number",
+                "order":"asc"
+            }
+        }
+        }
+        headers = { "Content-Type": "application/json" }
+
+        r = requests.get(url, auth=awsauth, headers=headers, data=json.dumps(query))
+
+        response = {
+        "statusCode": 200,
+        "headers": {
+            "Access-Control-Allow-Origin": '*'
+            },
+        "isBase64Encoded": False
+        }
+
+        response['body'] = r.text
+        # print(response['body'])
+        for item in json.loads(response['body'])['hits']['hits']:
+            if item['_source']['objectKey'] not in photos:
+                photos.append(item['_source']['objectKey'])
+    # print(photos)
+    return photos
+
+
+def lambda_handler(event, context):
+    os.environ['TZ'] = 'America/New_York'
+    time.tzset()
+    lex = boto3.client('lex-runtime')
+    # print(event)
+    keywords = []
+    lex_res = lex.post_text(botName='photosearch',
+                                botAlias='photosearch',
+                                userId='testuser',
+                                inputText=event['queryStringParameters']['q'])
+    for item in lex_res['slots'].values():
+        if item:
+            if item.lower() not in keywords:
+                keywords.append(item.lower())
+    # print(keywords)
+    photos = searchFromES(keywords)
